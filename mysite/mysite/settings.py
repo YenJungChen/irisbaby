edf62df14faad62d479c888a9df9639d41b4f454
irisbaby <candyfairest@hotmail.com>
2018-03-21 23:00:43 -0500

    use post function

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -106,13 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Taipei'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)

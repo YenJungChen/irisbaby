f5df816f5989f1c02629e450c4f7bb74238b6f85
irisbaby <candyfairest@hotmail.com>
2018-03-22 06:41:34 -0500

    add login/logout

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    
 ]
